9236d38d7356a1d4afa7631f2873d6b2c1bcd9a4
Ferhad <mislim.sheridancollege.ca>
2022-04-13 14:30:10 -0400

    added deploy.js

---
+++ redemption/scripts/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    const Redemption = await ethers.getContractFactory("Redemption");
+    const redemption = await Redemption.deploy();
+  
+    console.log("Redemption address:", redemption.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
