15ba740e667f4c8e4df25e0ff943fae1ba55f05c
Ferhad <mislim.sheridancollege.ca>
2022-04-13 15:59:55 -0400

    added hardhat.config.js

--- chance/hardhat.config.js
+++ chance/hardhat.config.js
@@ -1,6 +1,24 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('@openzeppelin/hardhat-upgrades');
+require("dotenv").config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.7",
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${PRIVATE_KEY}`]
+    }
+  },
+  etherscan:{
+    apiKey: ETHERSCAN_API_KEY
+  }
 };
